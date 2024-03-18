e150ca066af1b59ba26e2f56a5258d0b1da727a6
Blend Emini <blendxemini@gmail.com>
2024-03-18 02:00:48 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Signup from "./routes/Signup";
+import Signin from "./routes/Signin";
+import About from "./routes/About";
+import Profile from "./routes/Profile";
 function App() {
-    return <div className="text-red-600"></div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <div className="">Homeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</div>;
+};
+
+export default Home;
